5ca3393f45b24a5a44a691846f86dc4648d6e0de
Eliran Levy <eliran.levy@elevy-mc.local>
2017-04-21 14:19:04 +0300

    Initial Commit

---
+++ EliranFrog/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EliranFrog
+//
+//  Created by Eliran Levy on 21/04/2017.
+//  Copyright © 2017 Eliran Levy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
